21bdc3391cf259b3601924101bae1ffe55dbdf57
Cesar Aguilar <Blueavian9@gmail.com>
2026-01-04 20:36:21 -0800

    updated env.ts file

--- src/server/types/env.ts
+++ src/server/types/env.ts
@@ -1,8 +1,5 @@
 import type { D1Database } from "@cloudflare/workers-types";
 
-/**
- * Environment bindings injected by Cloudflare Workers
- */
 export type Env = {
-  DB: D1Database;
+	DB: D1Database;
 };
